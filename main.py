4530f0df837dfef447798fb92cd199356bfa78df
ortegajohn <ortegajohn@outlook.com>
2023-05-07 20:50:20 -0400

    update

--- main.py
+++ main.py
@@ -16,9 +16,25 @@ class MyRoot(BoxLayout):
 
     def __init__(self):
         super(MyRoot, self).__init__()
+        self.dice_roll = self.roll_dice
+        self.n_dice_rolls = self.roll_N_dice
+        self.dice_count = 3
+
+    def spinner_clicked(self, value):
+        self.dice_count = int(value)
+        self.ids.dropdown.text = value
 
     def generate_number(self):
-        self.random_label.text = str(random.randint(0, 1000))
+        self.random_label.text = str(self.roll_N_dice(n=self.dice_count))
+
+    def roll_dice(self):
+        return random.choice(range(1, 7))
+
+    def roll_N_dice(self, n=3):
+        roll_lst = []
+        for i in range(n):
+            roll_lst.append(self.dice_roll())
+        return roll_lst
 
 
 class NeuralRandom(App):
